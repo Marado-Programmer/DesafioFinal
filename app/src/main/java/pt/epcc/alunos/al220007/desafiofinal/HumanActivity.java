9d2e7160d61f38196556339b07a9d9b3a2573f42
Marado-Programmer <marado.pen.eng@gmail.com>
2022-11-24 09:36:44 +0000

    start

---
+++ app/src/main/java/pt/epcc/alunos/al220007/desafiofinal/HumanActivity.java
@@ -0,0 +1,9 @@
+package pt.epcc.alunos.al220007.desafiofinal;
+
+import android.support.v7.app.AppCompatActivity;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+abstract public class HumanActivity extends AppCompatActivity {
+	RecyclerView.Adapter adapter;
+}
--- app/src/main/java/pt/epcc/alunos/al220007/desafiofinal/MainActivity.java
+++ app/src/main/java/pt/epcc/alunos/al220007/desafiofinal/MainActivity.java
@@ -2,12 +2,29 @@ package pt.epcc.alunos.al220007.desafiofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+	private Button teacherBtn, studentBtn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		this.findViews();
+	}
+
+	private void findViews() {
+		this.teacherBtn = this.findViewById(R.id.teacherBtn);
+		this.studentBtn = this.findViewById(R.id.studentBtn);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v == this.teacherBtn) {
+
+		}
 	}
 }
